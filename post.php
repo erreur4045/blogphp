e2884b0096d3fa7fadf067fdd187ab85099079f3
maxime <maximethi@hotmail.fr>
2019-05-15 16:33:17 +0200

    test autolaoder

--- post.php
+++ post.php
@@ -1,18 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: maxime
- * Date: 01/04/2019
- * Time: 15:30
- */
-require('models/model.php');
 
-if (isset($_GET['id']) && $_GET['id'] > 0) {
-    $post = getPost($_GET['id']);
-    $comments = getComments($_GET['id']);
-    require('view/PostViewco.php');
-}
-else {
-    echo 'Erreur : aucun identifiant de billet envoyé';
+//Comment::test();
+
+function chagerclass($class){
+    require 'models/'.$class.'.php';
 }
 
+spl_autoload_register('chagerclass');
+
+$test = new Comment();
+$test->test();
