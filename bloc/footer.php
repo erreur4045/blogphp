0005745d43ba0dd1cb30b11bcde74819d8fec76b
maxime <maximethi@hotmail.fr>
2019-05-23 20:47:03 +0200

    ajout scprit js pour etape de validation pour suppression articles et commentaires

--- bloc/footer.php
+++ bloc/footer.php
@@ -5,6 +5,15 @@
             <div class="col-md-12">
                 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
                 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+                <script type="text/javascript">
+                    var elems = document.getElementsByClassName('confirmation');
+                    var confirmIt = function (e) {
+                        if (!confirm('Etes vous sur de vouloir supprimer?')) e.preventDefault();
+                    };
+                    for (var i = 0, l = elems.length; i < l; i++) {
+                        elems[i].addEventListener('click', confirmIt, false);
+                    }
+                </script>
                 <br>
                 <div class="footer-copyright text-center ">© 2019 Copyright:
                     <a href="https://www.linkedin.com/in/maxime-thierry-93870a97/"> maximethierry.xyz</a>
