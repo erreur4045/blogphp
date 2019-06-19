b4587d33f6e36e3c1058fc8062815ef06bba7e97
maxime <maximethi@hotmail.fr>
2019-06-19 13:37:05 +0200

    cleanage code

--- index.php
+++ index.php
@@ -20,80 +20,75 @@ require 'controleurs/ControleursComments.php';
 require 'models/DatabaseConnection.php';
 require 'models/CommentManager.php';
 
-$maintenance = 0;
-if ($maintenance == 0) {
-    if (isset($_GET['action'])) {
-        if ($_GET['action'] == 'listPosts') {
-            listPosts();
-        } elseif ($_GET['action'] == 'post') {
-            if (isset($_GET['id']) && $_GET['id'] > 0) {
-                post();
-            } else {
-                echo 'Erreur : aucun identifiant de billet envoyé';
-            }
-        } elseif ($_GET['action'] == 'listAllPosts') {
-            allPost();
-        } elseif ($_GET['action'] == 'contacter') {
-            contacter();
-        } elseif ($_GET['action'] == 'comment') {
-            comment();
-        } elseif ($_GET['action'] == 'modifcomment') {
-            modifcomment();
-        } elseif ($_GET['action'] == 'modifpost') {
-            modifpost();
-        } elseif ($_GET['action'] == 'validupdatepost') {
-            validupdatepost();
-        } elseif ($_GET['action'] == 'connection') {
-            connection();
-        } elseif ($_GET['action'] == 'connectionuser') {
-            connectionuser();
-        } elseif ($_GET['action'] == 'contact') {
-            contact();
-        } elseif ($_GET['action'] == 'updatecomment') {
-            updatecomm();
-        } elseif ($_GET['action'] == 'adminusertobevalided') {
-            adminusertobevalided();
-        } elseif ($_GET['action'] == 'adminuserlist') {
-            adminuserlist();
-        } elseif ($_GET['action'] == 'accceptuser') {
-            accceptuser();
-        } elseif ($_GET['action'] == 'suppuser') {
-            suppuser();
-        } elseif ($_GET['action'] == 'CV' OR $_GET['action'] == 'cv') {
-            CV();
-        } elseif ($_GET['action'] == 'inscription') {
-            incription();
-        } elseif ($_GET['action'] == 'deconnection') {
-            deconnection();
-        } elseif ($_GET['action'] == 'dashboard') {
-            dashboard();
-        } elseif ($_GET['action'] == 'dashboardcom') {
-            dashboard2();
-        } elseif ($_GET['action'] == 'dashboardcomtovalidated') {
-            dashboard3();
-        } elseif ($_GET['action'] == 'addnewpost') {
-            addnewpost();
-        } elseif ($_GET['action'] == 'supprpost') {
-            supprpost();
-        } elseif ($_GET['action'] == 'validpost') {
-            validpost();
-        } elseif ($_GET['action'] == 'validcomment') {
-            validcomment();
-        } elseif ($_GET['action'] == 'supprcom') {
-            supprcom();
-        } elseif ($_GET['action'] == 'validinscription') {
-            validincription();
-        } elseif ($_GET['action'] == 'testfunction') {
-            testfunction();
-        } elseif ($_GET['action'] == 'bio') {
-            bio();
-        } elseif ($_GET['action'] == 'testmail') {
-            testmail();
+if (isset($_GET['action'])) {
+    if ($_GET['action'] == 'listPosts') {
+        listPosts();
+    } elseif ($_GET['action'] == 'post') {
+        if (isset($_GET['id']) && $_GET['id'] > 0) {
+            post();
+        } else {
+            echo 'Erreur : aucun identifiant de billet envoyé';
         }
-    } else {
-        accueil();
+    } elseif ($_GET['action'] == 'listAllPosts') {
+        allPost();
+    } elseif ($_GET['action'] == 'contacter') {
+        contacter();
+    } elseif ($_GET['action'] == 'comment') {
+        comment();
+    } elseif ($_GET['action'] == 'modifcomment') {
+        modifcomment();
+    } elseif ($_GET['action'] == 'modifpost') {
+        modifpost();
+    } elseif ($_GET['action'] == 'validupdatepost') {
+        validupdatepost();
+    } elseif ($_GET['action'] == 'connection') {
+        connection();
+    } elseif ($_GET['action'] == 'connectionuser') {
+        connectionuser();
+    } elseif ($_GET['action'] == 'contact') {
+        contact();
+    } elseif ($_GET['action'] == 'updatecomment') {
+        updatecomm();
+    } elseif ($_GET['action'] == 'adminusertobevalided') {
+        adminusertobevalided();
+    } elseif ($_GET['action'] == 'adminuserlist') {
+        adminuserlist();
+    } elseif ($_GET['action'] == 'accceptuser') {
+        accceptuser();
+    } elseif ($_GET['action'] == 'suppuser') {
+        suppuser();
+    } elseif ($_GET['action'] == 'CV' OR $_GET['action'] == 'cv') {
+        CV();
+    } elseif ($_GET['action'] == 'inscription') {
+        incription();
+    } elseif ($_GET['action'] == 'deconnection') {
+        deconnection();
+    } elseif ($_GET['action'] == 'dashboard') {
+        dashboard();
+    } elseif ($_GET['action'] == 'dashboardcom') {
+        dashboard2();
+    } elseif ($_GET['action'] == 'dashboardcomtovalidated') {
+        dashboard3();
+    } elseif ($_GET['action'] == 'addnewpost') {
+        addnewpost();
+    } elseif ($_GET['action'] == 'supprpost') {
+        supprpost();
+    } elseif ($_GET['action'] == 'validpost') {
+        validpost();
+    } elseif ($_GET['action'] == 'validcomment') {
+        validcomment();
+    } elseif ($_GET['action'] == 'supprcom') {
+        supprcom();
+    } elseif ($_GET['action'] == 'validinscription') {
+        validincription();
+    } elseif ($_GET['action'] == 'testfunction') {
+        testfunction();
+    } elseif ($_GET['action'] == 'bio') {
+        bio();
+    } elseif ($_GET['action'] == 'testmail') {
+        testmail();
     }
 } else {
-    include 'bloc/maintenance.php';
+    accueil();
 }
 ?>
